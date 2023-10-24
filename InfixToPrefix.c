6cd6e23b4f1b5758a619f254282a93f289d39b7b
Suraj Gunagi <141156941+Surajgunagi7@users.noreply.github.com>
2023-10-24 23:53:48 +0530

    Update InfixToPrefix.c
    I Have Changed Entire code because the String reverse  function is not working on telnet so i have written a separate function

--- InfixToPrefix.c
+++ InfixToPrefix.c
@@ -7,27 +7,37 @@
 char stack[MAX_SIZE];
 int top = -1;
 
-void push(char item) {         // Function to push an item onto the stack
+void push(char item) {         /* Function to push an item onto the stack */
     if(top == MAX_SIZE - 1) 
         printf("Stack Overflow\n");
      else 
         stack[++top] = item;      /*Incrementing the index and then assigning the incoming value*/
 }
-char pop() {           // Function to pop an item from the stack
+char pop() {           /* Function to pop an item from the stack */
     if (top == -1) 
         printf("Stack Underflow\n");
     else 
         return stack[top--];    /*The value is Used First and then index is Decremented*/
 }
-/* To check whether the a character is number/Alphabet*/
+void reverse(char *str,int len)
+{
+    int i,temp;
+    for(i = 0; i < len/2; i++)
+    {
+        temp = str[i];
+        str[i] = str[len -i-1];
+        str[len-i-1] = temp;
+    }
+}
+/* To check whether the a character is number/Alphabet */
 int isAlphaNum(char x) { 
      return ((x >= 'A' && x <= 'Z') || (x >= 'a' && x <= 'z') || (x >= '0' && x <= '9'));
 }
-/* To check wether character is operator or not*/
+/* To check wether character is operator or not */
 int isOperator(char symbol) { 
     return (symbol == '+' || symbol == '-' || symbol == '*' || symbol == '/' || symbol == '^');
 }
-// Function to determine the precedence of an operator
+/* Function to determine the precedence of an operator */
 int precedence(char symbol) { 
     switch(symbol)
     {
@@ -39,33 +49,33 @@ int precedence(char symbol) {
         default : return 0;
     }
 }
-// Function to convert infix expression to prefix expression
+/* Function to convert infix expression to prefix expression */
 void infixToPrefix(char infix[], char prefix[]) 
 {
     int i, j = 0;
-    strrev(infix);        // Reverse the infix expression
+    reverse(infix,strlen(infix));        /* Reverse the infix expression */
         for(i = 0; i < strlen(infix); i++) 
         {
             if(infix[i] == ' ')
                 continue;
-            else if(isAlphaNum(infix[i]))  // If alphanumeric, add to the prefix expression
+            else if(isAlphaNum(infix[i]))  /* If alphanumeric, add to the prefix expression */
                 prefix[j++] = infix[i];
-            else if(infix[i] == ')')    // If closing parenthesis, push onto the stack
+            else if(infix[i] == ')')    /* If closing parenthesis, push onto the stack */
                     push(infix[i]);
                 else if(infix[i] == '(') 
-                    {   // If opening parenthesis, pop operators from the stack and add to prefix until a closing parenthesis is encountered
+                    {   /* If opening parenthesis, pop operators from the stack and add to prefix until a closing parenthesis is encountered */
                         while(stack[top] != ')') 
                         {
                             prefix[j++] = pop();
                         }
-                        pop(); // Pop the closing parenthesis
+                        pop(); /* Pop the closing parenthesis */
                     } 
                     else if(isOperator(infix[i])) 
                         {
                             if(precedence(stack[top]) > precedence(infix[i]))
                             {
-                                     // If operator, handle operator precedence and stack operations
-                                while(precedence(stack[top]) >= precedence(infix[i])) {
+                                     /* If operator, handle operator precedence and stack operations */
+                                while(precedence(stack[top]) > precedence(infix[i])) {
                                     prefix[j++] = pop();
                                 }
                                 push(infix[i]);
@@ -79,28 +89,27 @@ void infixToPrefix(char infix[], char prefix[])
                                         else
                                             push(infix[i]);
                                     }
-                                else
+                            else
                                     push(infix[i]);
                         }
         }
-    while (top != -1)  // Pop remaining operators from the stack and add to prefix
+    while (top != -1)  /* Pop remaining operators from the stack and add to prefix */
     {
         prefix[j++] = pop();
     }
-  // Reverse the prefix expression to get the correct order
-    strrev(prefix);
+  /* Reverse the prefix expression to get the correct order */
+    reverse(prefix,strlen(prefix));
 }
-void main()     // Main function to read input and call infixToPrefix function
+void main()     
 {
-    char infix[MAX_SIZE], prefix[MAX_SIZE];
+    char infix[MAX_SIZE], prefix[MAX_SIZE] = {0};
 
-    // Read infix expression from the user
+    
     printf("\nEnter infix expression: ");
     gets(infix);
 
-    // Convert infix to prefix
+    /* Convert infix to prefix */
     infixToPrefix(infix, prefix);
 
-     // Print the resulting prefix expression
     printf("Prefix expression: %s\n", prefix);
 }
